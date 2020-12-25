80f7e85056a10dfda4b043737278d268076fc609
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-25 18:23:17 +0700

    refactor Emojis component

--- finish/emoji-searcher/src/components/Emojis/index.js
+++ finish/emoji-searcher/src/components/Emojis/index.js
@@ -8,26 +8,21 @@ import EmojiBox from '../EmojiBox'
 
 const Emojis = ({ emojisData, searchText }) => {
   const [filteredEmojis, setfilteredEmojis] = useState([])
-  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    setLoading(true)
     setfilteredEmojis(
       searchEmojis({
         emojisData,
         searchText
     }))
 
-    setLoading(false)
   }, [emojisData, searchText])
 
   return (
     <div className={styles.emojisGrid}>
-      {(!loading && filteredEmojis.length > 0) ? filteredEmojis.map((data, index) => (
+      {filteredEmojis.map((data, index) => (
         <EmojiBox key={index} title={data.title} symbol={data.symbol} />
-      )) : (
-        <p>woyu</p>
-      )}
+      ))}
     </div>
   )
 }
