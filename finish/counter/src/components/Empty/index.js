f03fa6f9381947145563d39084ec26a53fa76874
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-25 16:06:14 +0700

    Make Empty component

---
+++ finish/counter/src/components/Empty/index.js
@@ -0,0 +1,11 @@
+import styles from './Empty.module.css'
+
+const Empty = () => {
+  return (
+    <div className={styles.empty}>
+      Wah kosong nih...
+    </div>
+  )
+}
+
+export default Empty
