e9a88c4bcc90e1eb38765cbd9e9a1c391f01ae96
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-25 18:23:02 +0700

    remove unused code in App.js and delete App.css

--- finish/emoji-searcher/src/App.js
+++ finish/emoji-searcher/src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css';
 
 import Navbar from './components/Navbar'
 import Container from './components/Container'
