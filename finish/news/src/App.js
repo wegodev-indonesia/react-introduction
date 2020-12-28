5847405ab950b9c395ca1994c1b3634f5cbd1870
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-28 08:04:43 +0700

    Create inital news card components and data source

--- finish/news/src/App.js
+++ finish/news/src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import Navbar from './components/Navbar'
+import Container from './components/Container'
+import NewsList from './components/NewsList'
+
+import { getTechNews } from './services/getNews'
 
 function App() {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchTechNews = async () => {
+      setLoading(true)
+      const res = await getTechNews()
+
+      if (!res) {
+        setError(true)
+        return
+      }
+
+      setLoading(false)
+      setArticles(res.articles)
+    }
+
+    fetchTechNews()
+  }, [])
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error...</p>}
+        {articles.length > 0 && (
+          <NewsList articles={articles} />
+        )}
+      </Container>
+    </>
   );
 }
 
