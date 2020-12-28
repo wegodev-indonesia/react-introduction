68682499107ea400a3a8d6212e4baea5c242490e
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-28 08:03:25 +0700

    create getTechNews service for query data

---
+++ finish/news/src/services/getNews.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const NEWS_API_BASE_URL = process.env.REACT_APP_NEWS_API_BASE_URL
+const NEWS_API_KEY = process.env.REACT_APP_API_KEY
+
+export const getTechNews = async () => {
+  try {
+    /* To see available path options
+    go to News API docs
+    https://newsapi.org/docs/endpoints */
+    const path = '/everything'
+    const query =`?q=technology&apiKey=${NEWS_API_KEY}`
+    const endpointURL = `${NEWS_API_BASE_URL}${path}${query}`
+  
+    const res = await axios.get(endpointURL)
+
+    if (res.status === 200) {
+      return res.data
+    }
+  } catch (error) {
+    console.error('Error getting Tech News: ', error)
+  }
+}
