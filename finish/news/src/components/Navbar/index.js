087aff18e48e4062ec7ec60df1bb211091d3dc7d
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-31 12:16:21 +0700

    Improve Navbar to have categories slider

--- finish/news/src/components/Navbar/index.js
+++ finish/news/src/components/Navbar/index.js
@@ -1,12 +1,29 @@
+import { Link } from "react-router-dom";
+
 import styles from './Navbar.module.css'
 
+import { CATEGORIES } from '../../constants/categories'
 import newsIcon from '../../assets/news-icon.svg'
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <img className={styles.navIcon} src={newsIcon} alt="navbar icon" />
-      <h1 className={styles.navTitle}>NEWS</h1>
+      <div className={styles.navIconWrapper}>
+        <img className={styles.navIcon} src={newsIcon} alt="navbar icon" />
+        <h1 className={styles.navTitle}>NEWS</h1>
+      </div>
+
+      <div className={styles.categories}>
+        {CATEGORIES.map((category, index) => (
+          <Link
+            key={index}
+            to={`/${category.slug}`}
+            className={styles.category}
+          >
+            {category.name}
+          </Link>
+        ))}
+      </div>
     </nav>
   )
 }
---
+++ finish/news/src/constants/categories.js
@@ -0,0 +1,30 @@
+export const CATEGORIES = [
+  {
+    name: 'Apple',
+    slug: 'apple'
+  },
+  {
+    name: 'PlayStation',
+    slug: 'playstation'
+  },
+  {
+    name: 'Microsoft',
+    slug: 'microsoft'
+  },
+  {
+    name: 'Google',
+    slug: 'google'
+  },
+  {
+    name: 'Netflix',
+    slug: 'netflix'
+  },
+  {
+    name: 'Gaming',
+    slug: 'gaming'
+  },
+  {
+    name: 'Nintendo',
+    slug: 'nintendo'
+  },
+]
