f9ff0fab9410f7d535b4190adee8f62099b76472
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-31 11:48:15 +0700

    implement initial React Router

--- finish/news/src/components/NewsList/index.js
+++ finish/news/src/components/NewsList/index.js
@@ -5,39 +5,48 @@ import styles from './NewsList.module.css'
 
 import { formatDate } from '../../utils/formatDate'
 
-const NewsList = ({ articles }) => {
+const NewsList = ({ articles, category }) => {
   return (
     <div className={styles.newsList}>
-      {articles.map((article, index, arr) => (
-        <div key={index} className={classnames(styles.newsCard, {
-          [styles.newsCardGap]: !(arr.length === index + 1)
-        })}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.img}
-              src={article.urlToImage}
-              alt={`${article.title} thumbnail img`} />
-            <p className={styles.imgTitle}>{article.title}</p>
+      {articles.map((article, index, arr) => {
+        if (!article.urlToImage || !article.author || !article.content) {
+          // If urlToImage, content, and/or author from API response is null
+          // return nothing (not rendering React component)
+          return null
+        }
+        
+        return (
+          <div key={index} className={classnames(styles.newsCard, {
+            [styles.newsCardGap]: !(arr.length === index + 1)
+          })}>
+            <div className={styles.imgContainer}>
+              <img
+                className={styles.img}
+                src={article.urlToImage}
+                alt={`${article.title} thumbnail img`} />
+              <p className={styles.imgTitle}>{article.title}</p>
+            </div>
+
+            <div className={styles.newsCardContent}>
+
+              <p className={styles.newsCardDate}>{formatDate(article.publishedAt)}</p>
+              <p className={styles.newsCardAuthor}>
+                {`${article.author} | ${article.source.name}`}
+              </p>
+
+              <p className={styles.newsCardDesc}>{article.description}</p>
+
+            </div>
           </div>
-
-          <div className={styles.newsCardContent}>
-
-            <p className={styles.newsCardDate}>{formatDate(article.publishedAt)}</p>
-            <p className={styles.newsCardAuthor}>
-              {`${article.author} | ${article.source.name}`}
-            </p>
-
-            <p className={styles.newsCardDesc}>{article.description}</p>
-
-          </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
 
 NewsList.propTypes = {
-  articles: PropTypes.array
+  articles: PropTypes.array,
+  category: PropTypes.string
 }
 
 export default NewsList
--- finish/news/src/pages/App.js
+++ finish/news/src/pages/App.js
@@ -1,20 +1,27 @@
 import { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
 
-import Navbar from './components/Navbar'
-import Container from './components/Container'
-import NewsList from './components/NewsList'
+import Navbar from '../components/Navbar'
+import Container from '../components/Container'
+import NewsList from '../components/NewsList'
 
-import { getTechNews } from './services/getNews'
+import { getNews } from '../services/getNews'
 
 function App() {
   const [articles, setArticles] = useState([])
+  const [category, setCategory] = useState('')
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
+
+  const { id } = useParams()
+  const DEFAULT_SEARCH_QUERY = 'technology'
 
   useEffect(() => {
     const fetchTechNews = async () => {
       setLoading(true)
-      const res = await getTechNews()
+      const res = await getNews({
+        searchQuery: id || DEFAULT_SEARCH_QUERY
+      })
 
       if (!res) {
         setLoading(false)
@@ -24,10 +31,11 @@ function App() {
 
       setLoading(false)
       setArticles(res.articles)
+      setCategory(res.category)
     }
 
     fetchTechNews()
-  }, [])
+  }, [id])
 
   return (
     <>
@@ -36,7 +44,7 @@ function App() {
         {loading && <p>Loading...</p>}
         {error && <p>Error...</p>}
         {articles.length > 0 && (
-          <NewsList articles={articles} />
+          <NewsList articles={articles} category={category} />
         )}
       </Container>
     </>
