17e19c3643a33b3997248e74ef9e45d7666c9923
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-12-31 11:47:56 +0700

    change getTechNews to getNews services

--- finish/news/src/services/getNews.js
+++ finish/news/src/services/getNews.js
@@ -3,19 +3,24 @@ import axios from 'axios'
 const NEWS_API_BASE_URL = process.env.REACT_APP_NEWS_API_BASE_URL
 const NEWS_API_KEY = process.env.REACT_APP_API_KEY
 
-export const getTechNews = async () => {
+export const getNews = async ({ searchQuery = 'technology' }) => {
   try {
     /* To see available path options
     go to News API docs
     https://newsapi.org/docs/endpoints */
     const path = '/everything'
-    const query =`?q=technology&apiKey=${NEWS_API_KEY}`
+    const query =`?q=${searchQuery}&apiKey=${NEWS_API_KEY}`
     const endpointURL = `${NEWS_API_BASE_URL}${path}${query}`
   
     const res = await axios.get(endpointURL)
 
     if (res.status === 200) {
-      return res.data
+      const additionalObj = {
+        category: searchQuery
+      }
+      const resData = {...res.data, ...additionalObj}
+
+      return resData
     }
   } catch (error) {
     console.error('Error getting Tech News: ', error)
